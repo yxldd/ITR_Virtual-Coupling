9149f1f0dec50f98b39fee63366dfab38db3f200
yxldd <1186494038@qq.com>
2022-05-02 17:07:35 +0800

    update:get_pos

--- binary2dist.py
+++ binary2dist.py
@@ -1,19 +1,20 @@
 import cv2 as cv
 import numpy as np
-file = open('D:\\Pho\\dist.txt','a')
-src=cv.imread("D:\\Pho\\out.jpg")
+
+file = open('D:\\Pho\\dist.txt', 'w')
+src = cv.imread("D:\\Pho\\out.jpg")
 ret, binary = cv.threshold(src, 174, 255, cv.THRESH_BINARY)
-i=0
-j=0
-print(src.shape[0],src.shape[1])
-while i<300:
-    j=0
-    str1=""
-    while j<300:
-        str1=str1+str(src[i][j][1])+" "
-        j=j+1
-    file.write(str1+'\n')
-    i=i+1
-
-
-
+i = 299
+j = 0
+print(src.shape[0], src.shape[1])
+while i >= 0:
+    j = 0
+    while j < 150:
+        if (src[i][150 + j][1] <= 10):
+            file.write("y: " + str(i) + "    pos: " + str(150 + j) + " \n")
+            break
+        if (src[i][150 - j][1] <= 10):
+            file.write("y: " + str(i) + "    pos: " + str(150 - j) + " \n")
+            break
+        j = j + 1
+    i = i - 1
